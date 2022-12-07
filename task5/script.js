2aee0a795fb772d808cb01ecdbcb05d96c4ef784
Andy75 <lysow@yandex.ru>
2022-12-07 10:56:42 +0700

    fixed 2 eroor in css

--- task5/script.js
+++ task5/script.js
@@ -4,7 +4,7 @@ textInput.addEventListener('input',  function (event)  {
      this.textContent = event.target.value;
     duplicateField.innerText = this.value;
   })
-button.addEventListener('click', (event) =>{
+button.addEventListener('click', (event) => {
   // можно, конечно, усложнить решение: if (event.target.nodeName === 'BUTTON') {};
       console.log(duplicateField.textContent);
       event.stopPropagation();
